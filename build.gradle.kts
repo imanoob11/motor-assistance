0790dd93d17580f95f3adf53f36db25f103637d4
imanoob11 <jairuslazaro08@gmail.com>
2025-06-06 23:07:51 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,2 @@
-plugins {
-    kotlin("jvm") version kotlinVersion
-}
-
-buildscript {
-    repositories {
-        maven("https://plugins.gradle.org/m2/")
-    }
-    dependencies {
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.0.0")
-        classpath("com.google.code.gson:gson:2.10.1")
-    }
-}
-
-repositories {
-    mavenCentral()
-}
-
-subprojects {
-    repositories {
-        mavenCentral()
-    }
-
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-}
+minecraft_version = 1.21.4
+yarn_mappings = 1.21.4+build.1
